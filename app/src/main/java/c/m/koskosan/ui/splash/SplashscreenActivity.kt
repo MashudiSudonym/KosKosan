ca95dc888b23490b2a2ed6f6a7d15e465e8e5b52
masrobot <muhamadmashudiardiwinata@gmail.com>
2021-04-04 15:47:07 +0700

    fix location permission

--- app/src/main/java/c/m/koskosan/ui/splash/SplashscreenActivity.kt
+++ app/src/main/java/c/m/koskosan/ui/splash/SplashscreenActivity.kt
@@ -1,11 +1,15 @@
 package c.m.koskosan.ui.splash
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import c.m.koskosan.databinding.ActivitySplashscreenBinding
 import c.m.koskosan.ui.login.LoginActivity
 import c.m.koskosan.ui.main.MainActivity
+import c.m.koskosan.util.Constants
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -45,5 +49,41 @@ class SplashscreenActivity : AppCompatActivity() {
                 }
             }
         )
+    }
+
+    // check ACCESS FINE LOCATION and ACCESS COARSE LOCATION permission
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    // request permission for get location
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            Constants.PERMISSION_REQUEST_LOCATION
+        )
+    }
+
+    // permission result
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
