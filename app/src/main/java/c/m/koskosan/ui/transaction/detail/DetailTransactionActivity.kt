a7c7c0cb3f91781ad1a919877ab9d491c279f6e4
masrobot <muhamadmashudiardiwinata@gmail.com>
2021-04-22 22:15:10 +0700

    create DetailTransactionActivity.kt

---
+++ app/src/main/java/c/m/koskosan/ui/transaction/detail/DetailTransactionActivity.kt
@@ -0,0 +1,12 @@
+package c.m.koskosan.ui.transaction.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import c.m.koskosan.R
+
+class DetailTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_transaction)
+    }
+}
