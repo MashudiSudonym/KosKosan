555753a591110b336a8d4ef229cf92d825b2cf3c
masrobot <muhamadmashudiardiwinata@gmail.com>
2021-05-08 22:10:37 +0700

    reformat code

--- app/src/main/java/c/m/koskosan/vo/ResponseState.kt
+++ app/src/main/java/c/m/koskosan/vo/ResponseState.kt
@@ -5,7 +5,7 @@ package c.m.koskosan.vo
  */
 
 sealed class ResponseState<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?):ResponseState<T>(data)
-    class Error<T>(message: String?,data: T?=null):ResponseState<T>(data, message)
-    class Loading<T>(data: T?):ResponseState<T>(data)
+    class Success<T>(data: T?) : ResponseState<T>(data)
+    class Error<T>(message: String?, data: T? = null) : ResponseState<T>(data, message)
+    class Loading<T>(data: T?) : ResponseState<T>(data)
 }
